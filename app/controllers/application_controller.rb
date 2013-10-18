6dd3d9c417db14498135d9213ce7ddea0e83c7ba
Alexandr <ale88andr@gmail.com>
2013-10-18 16:32:02 +0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
   protect_from_forgery
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+    def record_not_found
+      render 'public/404', status: 404
+    end
 end
